55238c22d3ba6a925fb54cea48af99f2f8230ec7
abhishek <abhishek7gg7@gmail.com>
2025-01-11 05:43:29 +0000

    Build Test

--- netlify/functions/api.mjs
+++ netlify/functions/api.mjs
@@ -1,10 +1,23 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import puppeteer from "puppeteer";
 
 const api = express();
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+
+router.get("/screenshot", async (req, res) => {
+  const browser = await puppeteer.launch({
+    executablePath: process.env.CHROME_PATH,
+    headless: true,
+  });
+  const page = await browser.newPage();
+  await page.goto("https://example.com");
+  const screenshot = await page.screenshot({ type: "png" });
+  await browser.close();
+  res.setHeader("Content-Type", "image/png");
+  res.send(screenshot);
+});
 
 api.use("/api/", router);
 
